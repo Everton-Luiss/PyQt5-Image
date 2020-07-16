add9fb7ad055205a5ef29e5b79cc8ba75c00dc58
Everton-Luiss <elssufpa@gmail.com>
2020-07-16 16:01:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,56 @@
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel, QMainWindow
 import sys
 from PyQt5.QtGui import QPixmap
 
 
-class Window(QDialog):
+class Window(QMainWindow):
     def __init__(self): #método construtor
         super().__init__()
         self.title = "PyQt5 Adding Image To Label"
-        self.top = 200
+        self.top = 100
         self.left = 100
         self.width = 400
-        self.height = 300
+        self.height = 500
         self.iconName = "home.png"
+        #self.setStyleSheet("background-color:black;"
 
-        self.label_1 = QLabel(self)
-        self.label_1.setText("Registration Info")
-        self.label_1.move(450,80)
-        self.label_1.setStyleSheet('QLabel {font: bold; font-size:20px}')
-        self.label_1.resize(200,25)
+        #self.label_1 = QLabel(self)
+        #self.label_1.setText("Registration Info")
+        #self.label_1.move(450,80)
+        #self.label_1.setStyleSheet('QLabel {font: bold; font-size:20px; color: "black"}')
+        #self.label_1.resize(150,25)
+
         self.InitWindow()
 
     def InitWindow(self):
-        self.setWindowIcon(QtGui.QIcon(self.iconName))
+        self.label = QLabel(self)
+        self.label.setPixmap(QPixmap('nuvem2.png'))
+        self.setGeometry(100000,50, 500,10000)
+        
+        self.setWindowIcon(QtGui.QIcon("icon.png"))
         self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.setStyleSheet("QMainWindow {background: 'yellow';}");
         
-        vbox = QVBoxLayout()
 
-        labelImage = QLabel(self)
-        pixmap = QPixmap("nuvem2.png")
-        labelImage.setPixmap(pixmap)
-        vbox.addWidget(labelImage)
+        #vbox = QVBoxLayout()
+
+       # labelImage = QLabel(self)
+        #pixmap = QPixmap("nuvem2.png")
+        #labelImage.setPixmap(pixmap)
+        #self.setGeometry(60,50, 1000,400)
+        #vbox.addWidget(labelImage)
         
-        label = QLabel("This Is PyQt5 Labels")
-        vbox.addWidget(label)
-        label2 = QLabel("This Is PyQt5 GUI Applicaition Development, Hello")
-        label2.setFont(QtGui.QFont("Sanserif", 20))
-        label2.setStyleSheet('color:red')
-        vbox.addWidget(label2)
+        #label = QLabel("This Is PyQt5 Labels")
+        #vbox.addWidget(label)
+        #label2 = QLabel("This Is PyQt5 GUI Applicaition Development, Hello")
+        #label2.setFont(QtGui.QFont("Sanserif", 20))
+        #abel2.setStyleSheet('color:red')
+        #vbox.addWidget(label2)
 
         
-        self.setLayout(vbox)
+        #self.setLayout(vbox)
         self.show()
  
  
